76fce88db6ed3749d09c12ab098331fe86efca66
jaermaine <145512922+jaermaine@users.noreply.github.com>
2024-05-10 19:24:23 +0800

    created index.php file with contents

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Login Page -->
+
+    <form action = "login-validate.php" method = "POST">
+        <label for = "username">Username: </label>
+        <input type = "text" required><br>
+
+        <label for = "username">Password: </label>
+        <input type = "text" required><br>
+
+        <input type = "submit" value = "Login">
+    </form>
+</body>
+</html>
