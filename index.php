7473305262a5f283d0f9bd31ce983aa35a1fcf9c
jaermaine <145512922+jaermaine@users.noreply.github.com>
2024-05-10 20:09:31 +0800

    added div and create account reference

--- index.php
+++ index.php
@@ -5,17 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<>
     <!-- Login Page -->
 
-    <form action = "login-validate.php" method = "POST">
-        <label for = "username">Username: </label>
-        <input type = "text" name = "username" required><br>
+    <div>
+        <form action = "login-validate.php" method = "POST">
+            <label for = "username">Username: </label>
+            <input type = "text" name = "username" required><br>
 
-        <label for = "password">Password: </label>
-        <input type = "text" name = "password" required><br>
+            <label for = "password">Password: </label>
+            <input type = "text" name = "password" required><br>
 
-        <input type = "submit" name = "submit" value = "Login">
-    </form>
+            <input type = "submit" name = "submit" value = "Login">
+
+            <a href = "register.php">Create an account</a>
+        </form>
+    </div>
 </body>
 </html>
