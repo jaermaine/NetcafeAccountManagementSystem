010723faf8db8b28db280681f0a7242bb8926801
jaermaine <145512922+jaermaine@users.noreply.github.com>
2024-05-11 23:48:40 +0800

    added a database connection

--- login-validate.php
+++ login-validate.php
@@ -1,6 +1,8 @@
 <?php
-    if(isset($_POST["submit"])){
-        echo $_POST["username"];
-        echo $_POST["password"];
-    }
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "netcafe";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection Failed" . mysqli_connect_error());
 ?>
