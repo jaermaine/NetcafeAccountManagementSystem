f223f320f7c76c255595b74d9fe30725d22063cd
jaermaine <145512922+jaermaine@users.noreply.github.com>
2024-05-14 15:04:14 +0800

    added db.php

--- login-validate.php
+++ login-validate.php
@@ -8,18 +8,7 @@
         header("Location: index.php");   
     }
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "netcafe";
-
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-    $_SESSION['connect'] = $conn;
-
-    if(!$conn){
-        die("Connection Failed " . mysqli_connect_error());
-    }
+    include 'db.php';
 
     $user = $_POST["username"];
     $pass = $_POST["password"];
--- register-validate.php
+++ register-validate.php
@@ -1,6 +1,8 @@
 <?php
     session_start();
-    
+
+    include 'db.php';
+
     if(!isset($_POST['submit'])){
         header("Location: index.php");
     }
@@ -13,10 +15,10 @@
     $role = $_POST['role'];
     $deposit = $_POST['deposit'];
 
-    $query = "INSERT INTO Users(`user_id`, `username`, `user_password`, `first_name`, `last_name`, `role_id`, `status_id`) VALUE 
+    $query = "INSERT INTO users(`user_id`, `username`, `user_password`, `first_name`, `last_name`, `role_id`, `status_id`) VALUE 
     ({$role_id}, {$username}, {$password}, {$first_name}, {$last_name}, {$role_id}, 0, {$deposit});";
 
-    if(mysqli_query($_SESSION['connect'], $query)){
+    if(mysqli_query($conn, $query)){
        header("admin-page.php");
        $_SESSION['registration_message'] = "Account created"; 
     }else{
