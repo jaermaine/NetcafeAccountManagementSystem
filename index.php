faf793d3a12f0b565662707d9674b85663ab5eaf
jaermaine <145512922+jaermaine@users.noreply.github.com>
2024-05-14 22:50:17 +0800

    changed title, and linked css

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <link rel = "stylesheet" href = "styles.css">
 </head>
     <div class = "box">
 
