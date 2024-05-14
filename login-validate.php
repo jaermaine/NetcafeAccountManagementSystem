303aec544213e5f0aee91f1b4e1355ee72161874
jaermaine <145512922+jaermaine@users.noreply.github.com>
2024-05-14 13:55:23 +0800

    fix attempt

--- login-validate.php
+++ login-validate.php
@@ -2,8 +2,8 @@
     session_start();
 
     if(!isset($_POST['submit'])){
-        header("Location: index.php");
         session_destroy();
+        header("Location: index.php");
     }else{
 
     $servername = "localhost";
