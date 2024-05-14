9efc539cd60e2206f0ab60bdf9d3be4f72552b05
jaermaine <145512922+jaermaine@users.noreply.github.com>
2024-05-14 13:11:32 +0800

    initialized connect session variable

--- login-validate.php
+++ login-validate.php
@@ -8,6 +8,8 @@
 
     $conn = mysqli_connect($servername, $username, $password, $dbname);
 
+    $_SESSION['connect'] = $conn;
+
     if(!$conn){
         die("Connection Failed " . mysqli_connect_error());
     }
--- register-validate.php
+++ register-validate.php
@@ -1,4 +1,15 @@
 <?php
+    $role_id = uniqid('2024-');
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
-    $username = $_POST[]
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $role = $_POST['role'];
+    $deposit = $_POST['deposit'];
+
+    $query = "INSERT INTO Users(`user_id`, `username`, `user_password`, `first_name`, `last_name`, `role_id`, `status_id`) VALUE 
+    ({$role_id}, {$username}, {$password}, {$first_name}, {$last_name}, {$role_id}, 0, {$deposit});";
+
+    if(mysqli_query($conn, $query)){
+
+    }
