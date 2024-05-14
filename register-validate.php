906335a22d6fa15afb312b1f3f80af3058807c91
jaermaine <145512922+jaermaine@users.noreply.github.com>
2024-05-14 15:08:04 +0800

    fixed the query

--- register-validate.php
+++ register-validate.php
@@ -15,7 +15,7 @@
     $role = $_POST['role'];
     $deposit = $_POST['deposit'];
 
-    $query = "INSERT INTO users(`user_id`, `username`, `user_password`, `first_name`, `last_name`, `role_id`, `status_id`) VALUE 
+    $query = "INSERT INTO users(`user_id`, `username`, `user_password`, `first_name`, `last_name`, `role_id`, `status_id`, `remaining_hours`) VALUE 
     ({$role_id}, {$username}, {$password}, {$first_name}, {$last_name}, {$role_id}, 0, {$deposit});";
 
     if(mysqli_query($conn, $query)){
