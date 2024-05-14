80a83e80062a8142543051bc3c935237a170af57
jaermaine <145512922+jaermaine@users.noreply.github.com>
2024-05-14 14:42:50 +0800

    fixed $_POST

--- logout-validate.php
+++ logout-validate.php
@@ -1,6 +1,6 @@
 <?php
 
-    if(!isset($_POST['submit'])){
+    if(!isset($_POST['logout'])){
         header("Location: index.php");
     }
 
--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-    if(!isset($_POST['submit'])){
+    if(!isset($_POST['register'])){
         header("Location: index.php");
     }
 ?>
