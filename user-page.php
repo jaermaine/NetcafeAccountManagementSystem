131176b2931cab2d95002547ca233a8022c4b152
Jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2024-05-15 15:05:23 +0800

    fixed directory 2

--- user-page.php
+++ user-page.php
@@ -2,7 +2,7 @@
     session_start();
 
     if(!isset($_SESSION['login'])){
-        header("Location: \NetcafeAccountManagementSystem\index.php");
+        header("Location: index.php");
     }
 ?>
 
@@ -18,7 +18,7 @@
         echo "<h1>Welcome back " . $_SESSION['username'] . "</h1>";
     ?>
     
-    <form action = "\validate\logout-validate.php" method = "POST">
+    <form action = "validate\logout-validate.php" method = "POST">
         <input type = "submit" value = "Logout">
     </form>
 </body>
