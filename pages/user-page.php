88f6bf4a28337492f7e93195dd2561872e1f60d5
jaermaine <145512922+jaermaine@users.noreply.github.com>
2024-05-16 19:31:48 +0800

    fixed header dir

--- pages/user-page.php
+++ pages/user-page.php
@@ -2,7 +2,7 @@
     session_start();
 
     if(!isset($_SESSION['login'])){
-        header("Location: index.php");
+        header("Location: /NetcafeAccountManagementSystem/index.php");
     }
 ?>
 
