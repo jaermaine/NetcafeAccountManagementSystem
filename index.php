da8eb66809ca239eed177f81451152246d727d1b
jaermaine <145512922+jaermaine@users.noreply.github.com>
2024-05-31 21:58:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,29 +8,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel = "stylesheet" href = "style.css">
+    <!--<link rel = "stylesheet" href = "style.css">-->
 </head>
     <div class = "box">
+        <div class = "icon">
+            <img src = "frontend\profile.png" alt = "ICON">
+        </div>
 
+        <div class = "Login">
+                <form action = "validate\login-validate.php" method = "POST">
+                    <input type = "text" id = "Uname" name = "username" placeholder = "Username" required><br>
+
+                    <input type = "password" id = "Pass" name = "password" placeholder = "Password" required><br>
+                    <?php
+                        if(!empty($_SESSION['message'])){
+                            echo $_SESSION['message'];
+                        }
+                    ?>
+                    <input type = "submit" class = "button-30" name = "submit" value = "Login">
+                </form>
+            </div>
     </div>
 
-    <div class = "icon">
-        <img src = "frontend\profile.png" alt = "ICON">
-    </div>
+    
 
-    <div class = "Login">
-        <form action = "validate\login-validate.php" method = "POST">
-            <input type = "text" id = "Uname" name = "username" placeholder = "Username" required><br>
-
-            <input type = "password" id = "Pass" name = "password" placeholder = "Password" required><br>
-            <?php
-                if(!empty($_SESSION['message'])){
-                    echo $_SESSION['message'];
-                }
-            ?>
-            <input type = "submit" class = "button-30" name = "submit" value = "Login">
-        </form>
-    </div>
+    
 </body>
 </html>
 
