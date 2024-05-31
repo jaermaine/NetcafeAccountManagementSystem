fe181f0f2bcb35dfc7327a487db51a7590a8cbae
jaermaine <145512922+jaermaine@users.noreply.github.com>
2024-05-31 23:30:49 +0800

    added link to css

--- pages/admin-page.php
+++ pages/admin-page.php
@@ -15,6 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
     <link rel ="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel = "stylesheet" href = "style.css">
 </head>
 <body>
     <?php
--- pages/modification-page.php
+++ pages/modification-page.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration</title>
-    <!--<link rel = "stylesheet" href = "..\style.css">-->
+    <link rel = "stylesheet" href = "..\style.css">
 </head>
 <body>
     
