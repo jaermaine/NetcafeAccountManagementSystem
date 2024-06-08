5ae94776b52b8452344774d1b35153a71f21c4a8
oxsu <romuelborja09@gmail.com>
2024-06-08 13:39:37 +0800

    update remaining hours of account based on time activity

--- validate/logout-validate.php
+++ validate/logout-validate.php
@@ -1,11 +1,17 @@
 <?php
     include 'db.php';
+    session_start();
 
     if(!isset($_POST['logout'])){
         header("Location: ../index.php");
     }
-
     $user = $_POST['user_id'];
+
+    if($_SESSION['role']){
+        $remaining_time = $_SESSION['original_time'] - (time() - $_SESSION['time_start']); 
+        $update_time = "UPDATE account SET time = '$remaining_time' WHERE user_id = '$user'";
+        $conn->query($update_time);
+    }
 
     $active_user = "UPDATE account SET active = 0 WHERE user_id = '$user'";
     $conn->query($active_user);
