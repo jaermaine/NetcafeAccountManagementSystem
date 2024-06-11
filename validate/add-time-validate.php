c53bc253768a5dd72c90b3cd358987355d91e8a8
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2024-06-11 13:30:18 +0800

    moved add time validator

---
+++ validate/add-time-validate.php
@@ -0,0 +1,45 @@
+<?php
+    session_start();
+
+    include 'db.php';
+
+    if (!isset($_POST['submit-addtime'])) {
+    } else {
+
+        $userId = $_POST['user_id'];
+        $statusId = $_POST['status_id'];
+        $time = 0;
+
+        $services = "SELECT * FROM services WHERE service_name = '$statusId'";
+        $time = ($_POST['status_id'] == "Regular") ? $time = $_POST['add_hrs_regular'] : $time = $_POST['add_hrs_vip'];
+
+        $services_query = $conn->query($services);
+        $service_row = $services_query->fetch_assoc();
+        $service_id = $service_row['services_id'];
+        $service_date = date(DATE_COOKIE);
+
+        $amount = $time * $service_row['service_rate'];
+        $transaction = "INSERT INTO `transactions`(`user_id`, `services_id`, `amount`, `hours_added`, `date`) VALUES ('$userId','$service_id','$amount', '$time', '$service_date')";
+        $transaction_query = $conn->query($transaction);
+
+        $query = "SELECT time FROM account WHERE user_id = $userId";
+        $result = $conn->query($query);
+        $row = $result->fetch_assoc();
+        if (!$row) {
+            echo "Error fetching current hours: " . $conn->error;
+        }
+
+        $currentHours = $row['time'];
+        $newHours = $currentHours + ($time * 3600);
+
+        $update = "UPDATE account SET time = $newHours WHERE user_id = $userId";
+        $update_query = $conn->query($update);
+        if ($update_query === FALSE) {
+            echo "Error updating hours: " . $conn->error;
+        }
+        echo "<script language = 'JavaScript'>
+                            alert('Successfully Added Time');
+                            window.location = \"../pages/admin-page.php\";
+                            </script>";
+    }
+    ?>
