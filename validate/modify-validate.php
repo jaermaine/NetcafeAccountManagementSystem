252c6ea156a9cb58b13aeb4f22a3d864d314a48e
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2024-06-11 14:10:22 +0800

    removed password, role, initialized change, if username exist alert(), optimized else

--- validate/modify-validate.php
+++ validate/modify-validate.php
@@ -30,42 +30,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     }
 
     $username = $row["username"];
-    $password = password_verify('', $row["password"]);
     $first_name = $row["first_name"];
     $last_name = $row["last_name"];
-    $role = $row["role_id"];
+    $password = $row["password"];
     $status = $row['status_id'];
-
 } else {
     // POST method: update the data off the client
+    $change = "";
     $user_id = $_POST["user_id"];
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+    $username = isset($_POST['username']) ? $_POST['username'] : $_POST['default_username'];
     $first_name = $_POST["first_name"];
     $last_name = $_POST["last_name"];
-    $role = $_POST["role_id"];
-    $status = $_POST['status_id'];
+    $status = $_POST['status_id']; 
 
-    // for finding if may same username sa database 
-    $sql_u = "SELECT * FROM account WHERE username='$username'";
-    $res_u = $conn->query($sql_u);
-    $array = mysqli_fetch_assoc($res_u);
-
-    if (checkUsername($conn, $_POST['username'])) {
+    if (!checkUsernameValidity($conn, $username)) {
         // updates details to the database
+        echo "<script language = 'JavaScript'>
+                            alert('Username Taken');
+                            window.location = \"../pages/modification-page.php?user_id=$user_id\";
+                            </script>";
+        exit;
+    } else {
         $update = "UPDATE account
-            SET username = '$username', password = '$password', first_name = '$first_name', last_name = '$last_name', role_id = '$role' WHERE user_id = $user_id";
+            SET first_name = '$first_name', last_name = '$last_name', status_id = '$status' WHERE user_id = $user_id";
         $results = $conn->query($update);
-
-        header("location: create.php");
-
         if (!$results) {
             echo "Invalid Query: " . $conn->error;
         }
-
         $_SESSION['registration_message'] = "Successfully updated user's details";
-
         header("location: ../pages/admin-page.php");
-
     }
 }
