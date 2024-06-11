cf75dcee1442125c0f456cd660ab9d321bdfa44e
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2024-06-11 13:39:45 +0800

    optimized username checker for new accounts

--- validate/register-validate.php
+++ validate/register-validate.php
@@ -4,19 +4,19 @@ session_start();
 include 'db.php';
 include 'functions.php';
 
-if (!isset($_POST['submit'])) {
+/*if (!isset($_POST['submit'])) {
     header("Location: ../index.php");
-}
+}*/
 
-$usercheck_query = "SELECT COUNT(username) AS numrows FROM account WHERE username = '" . $_POST['username'] . "'";
-$usercheck_result = mysqli_query($conn, $usercheck_query);
-$usercheck_row = mysqli_fetch_assoc($usercheck_result);
-if (checkUsername($conn, $_POST['username'])) {
+if (!checkUsernameValidity($conn, $_POST['username'])) {
+    header("Location: ../pages/admin-page.php");
+    $_SESSION['registration_message'] = "Username Taken";
+} else {
 
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $password = isset($_POST['password']) ? password_hash($_POST['password'], PASSWORD_DEFAULT) : null;
     $role = $_POST['role'];
     $status = $_POST['status'];
     $deposit = $_POST['deposit'];
@@ -25,10 +25,12 @@ if (checkUsername($conn, $_POST['username'])) {
     ('{$first_name}', '{$last_name}', '{$username}', '{$password}', '{$deposit}', '{$role}', '{$status}');";
 
 
-    if (mysqli_query($conn, $query)) {
-        header("Location: ../pages/admin-page.php");
-        $_SESSION['registration_message'] = "Account created";
-    } else {
-        header("Location:" . $_SESSION['referer']);
+    $username_check_query = $conn->query($query);
+
+    if (!$username_check_query) {
+        header("Location: ../pages/registration-page.php");
     }
+
+    header("Location: ../pages/admin-page.php");
+    $_SESSION['registration_message'] = "Account created";
 }
