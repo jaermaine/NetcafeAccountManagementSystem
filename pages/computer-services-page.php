10adc9607c66995ce1c69b9830af34f37a0abc18
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2024-06-14 23:04:23 +0800

    removed random text

--- pages/computer-services-page.php
+++ pages/computer-services-page.php
@@ -12,7 +12,7 @@ $vip = $time_result[1][2];
 <!DOCTYPE html>
 <html lang="en">
 
-<>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
