0c5ebef38e91bde9ac4b7c5d2e102e56dd552348
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2024-06-14 12:53:34 +0800

    fix modal

--- pages/computer-services-page.php
+++ pages/computer-services-page.php
@@ -2,17 +2,22 @@
 session_start();
 
 include '../validate/db.php';
+include '../validate/functions.php';
+
+$time_result = retrieveServices($conn);
+
+$regular = $time_result[0][2];
+$vip = $time_result[1][2];
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+<>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../style.css">
     <title>Computer Services - Staff</title>
-    <!-- back button -->
-    <a class='btn btn-primary btn-sm' href='../pages/staff-page.php'>Return</a>
     <!-- CSS -->
     <style>
         /* The Modal (background) */
@@ -63,40 +68,46 @@ include '../validate/db.php';
             cursor: pointer;
         }
     </style>
-</head>
+    </head>
 
-<body>
-    <h2> List of Users</h2>
-    <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
+    <body>
+        <div class="container mt-5">
 
-    <br>
-    <table class="table" id="userTable">
-        <tr>
-            <th>User ID</th>
-            <th>Username</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Status ID</th>
-            <th>Remaining Time</th>
-            <th>Action</th>
-        </tr>
-        <?php
-        // read all row from the database table
-        $sql = "SELECT DISTINCT account.user_id AS 'user_id', account.username AS 'Username', account.first_name AS 'FirstName', account.last_name AS 'LastName', status.status_name AS Status, account.time AS Time FROM account 
+            <?php
+            if (isset($_SESSION['registration_message'])) echo "<h4 class='text-center'>(" . $_SESSION['registration_message'] . ")</h4><br>";
+            ?>
+            <h2> List of Users</h2><br> 
+            <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
+
+            <br>
+            <table class="tablee" id="userTable">
+                <tr>
+                    <th>User ID</th>
+                    <th>Username</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Status ID</th>
+                    <th>Remaining Time</th>
+                    <th>Action</th>
+                </tr>
+                <?php
+                // read all row from the database table
+                $sql = "SELECT DISTINCT account.user_id AS 'user_id', account.username AS 'Username', account.first_name AS 'FirstName', account.last_name AS 'LastName', roles.role_name AS Role, status.status_name AS Status, account.time AS Time FROM account 
+            JOIN roles ON account.role_id = roles.role_id 
             JOIN status ON account.status_id = status.status_id
             -- query to exclude non - customer in status and role is admin and staff to limit staff actions
             WHERE account.role_id NOT IN (1, 2) 
             ORDER BY Status DESC";
-        $results = $conn->query($sql);
+                $results = $conn->query($sql);
 
-        if (!$results) {
-            die("Invalid query: " . $conn->error);
-        }
+                if (!$results) {
+                    die("Invalid query: " . $conn->error);
+                }
 
-        //read data from each row
-        while ($row = $results->fetch_assoc()) {
-            $row['Time'] = number_format(($row['Time'] / 3600));
-            echo " <tr>
+                //read data from each row
+                while ($row = $results->fetch_assoc()) {
+                    $row['Time'] = number_format(($row['Time'] / 3600));
+                    echo " <tr>
             <td>$row[user_id]</td>
             <td>$row[Username]</td>
             <td>$row[FirstName]</td>
@@ -106,13 +117,19 @@ include '../validate/db.php';
             <td>
                 <button class='btn btn-primary btn-sm add-time-btn' 
                 data-user-id='{$row['user_id']}' 
-                data-username='{$row['Username']}' 
+                data-username='{$row['Username']}'
+                data-role='{$row['Role']}' 
                 data-status='{$row['Status']}'>Add Time</button>
             </td>
         </tr>
             ";
-        }
-        ?>
+                }
+                ?>
+                </tbody>
+            </table>
+            <!-- back button -->
+            <a class='button' href='../pages/staff-page.php'>Return</a>
+        </div>
 
         <!-- JS for search functionality -->
         <script>
@@ -148,16 +165,15 @@ include '../validate/db.php';
                 <span class="close">&times;</span>
                 <h3 id="modal-title-regular">Add Time</h3>
                 <p id="text-regular"></p>
-                <form action="../validate/add-time-validate.php" method="POST" id="RegularForm">
+                <form action="../validate/add-time-validate.php" method="POST">
                     <input type="hidden" id="user_id_regular" name="user_id">
                     <input type="hidden" id="status_id_regular" name="status_id" value="Regular">
-                    <p>Rate: 50php/hour</p>
+                    <p>Rate: <?php echo $regular; ?>php/hour</p>
                     <label for="add_hrs_regular">Enter additional hours:</label>
                     <input type="number" id="add_hrs_regular" name="add_hrs_regular" min="0" required>
                     <br><br>
                     <p>Amount to be paid: ₱<span id="regular-amount">0</span></p>
-                    <br>
-                    <input type="submit" name="submit-addtime" value="Submit">
+                    <input type="submit" name="submit-addtime" class="button" value="Submit">
                 </form>
             </div>
         </div>
@@ -169,16 +185,16 @@ include '../validate/db.php';
                 <span class="close">&times;</span>
                 <h3 id="modal-title-vip">Add Time</h3>
                 <p id="text-vip"></p>
-                <form action="../validate/add-time-validate.php" method="POST" id="VIPForm">
+                <form action="../validate/add-time-validate.php" method="POST">
                     <input type="hidden" id="user_id_vip" name="user_id">
                     <input type="hidden" id="status_id_vip" name="status_id" value="VIP">
-                    <p>Rate: 30php/hour</p>
+                    <p>Rate: <?php echo $vip; ?>php/hour</p>
                     <label for="add_hrs_vip">Enter additional hours:</label>
                     <input type="number" id="add_hrs_vip" name="add_hrs_vip" min="0" required>
                     <br><br>
                     <p>Amount to be paid: ₱<span id="vip-amount">0</span></p>
                     <br>
-                    <input type="submit" name="submit-addtime" value="Submit">
+                    <input type="submit" class="button" name="submit-addtime" value="Submit">
                 </form>
             </div>
         </div>
@@ -198,21 +214,19 @@ include '../validate/db.php';
             var VIPForm = document.getElementById("VIPForm");
 
             // When the user clicks on <span> (x), close the modal
-            RegularSpan.onclick = function () {
+            RegularSpan.onclick = function() {
                 RegularModal.style.display = "none";
-                RegularForm.reset();
             }
-            VIPSpan.onclick = function () {
+            VIPSpan.onclick = function() {
                 VIPModal.style.display = "none";
-                VIPForm.reset();
             }
 
             // Get all the buttons that open the modal
             var buttons = document.getElementsByClassName("add-time-btn");
 
             // Add event listener to each button
-            Array.from(buttons).forEach(function (button) {
-                button.addEventListener('click', function () {
+            Array.from(buttons).forEach(function(button) {
+                button.addEventListener('click', function() {
                     var userId = this.getAttribute('data-user-id');
                     var username = this.getAttribute('data-username');
                     var role = this.getAttribute('data-role');
@@ -226,33 +240,32 @@ include '../validate/db.php';
                             RegularModal.style.display = "block";
 
                             // Listen for changes in the add_hrs_regular input field
-                            document.getElementById('add_hrs_regular').addEventListener('input', function () {
+                            document.getElementById('add_hrs_regular').addEventListener('input', function() {
                                 var reg_hours = this.value;
-                                var reg_rate = <?php echo $time_result[0][2]; ?>; // 50php/hour
+                                var reg_rate = <?php echo $regular; ?>; // 50php/hour
                                 var reg_amount = reg_hours * reg_rate;
                                 document.getElementById('regular-amount').innerText = reg_amount;
                             });
                             break;
 
-                        // for VIP
+                            // for VIP
                         case "VIP":
                             document.getElementById('user_id_vip').value = userId;
                             document.getElementById('modal-title-vip').innerText = "Add Time: " + username + " | Role: " + role + " | Status: " + status;
                             VIPModal.style.display = "block";
 
                             // Listen for changes in the add_hrs_vip input field
-                            document.getElementById('add_hrs_vip').addEventListener('input', function () {
+                            document.getElementById('add_hrs_vip').addEventListener('input', function() {
                                 var vip_hours = this.value;
-                                var vip_rate = <?php echo $time_result[1][2]; ?>; // 30php/hour
+                                var vip_rate = <?php echo $vip; ?>; // 30php/hour
                                 var vip_amount = vip_hours * vip_rate;
                                 document.getElementById('vip-amount').innerText = vip_amount;
-
                             });
                             break;
                     }
                 });
             });
         </script>
-</body>
+    </body>
 
 </html>
