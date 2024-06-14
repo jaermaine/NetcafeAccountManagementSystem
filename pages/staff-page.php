b3ce1c6fe47860b9d24da2dcde92f1faa96307c3
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2024-06-14 12:53:46 +0800

    link to stylesheet

--- pages/staff-page.php
+++ pages/staff-page.php
@@ -15,6 +15,8 @@ include '../validate/db.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Staff</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="..\style.css">
 </head>
 <style>
     body {
