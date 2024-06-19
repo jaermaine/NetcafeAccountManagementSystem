79e378be92fb4273edaeb314961c0698def29fca
jaermaine <jaermaine.domingcil@lpunetwork.edu.ph>
2024-06-20 00:40:05 +0800

    implement a password validator algorithm

--- validate/functions.php
+++ validate/functions.php
@@ -17,10 +17,41 @@ function checkUsernameValidity($conn, $username)
     }
 }
 
-function retrieveServices($conn){
+function retrieveServices($conn)
+{
     $services_query = "SELECT * FROM services";
     $services_result = $conn->query($services_query);
     $services_rows = $services_result->fetch_all();
 
     return $services_rows;
+}
+
+function validatePassword($conn, $password)
+{
+    if (strlen($password) < 6) {
+        return false;
+    }
+
+    if (preg_match('/\s/', $password)) {
+        return false;
+    }
+
+    $valid_characters = [
+        'alpha' => '/[A-z]/',
+        'numeral' => '/[0-9]/',
+        'non_alphanumeric' => '/\W/'
+    ];
+
+    $valid_ctr = 0;
+    foreach ($valid_characters as $key => $valid) {
+        if (preg_match($valid, $password)) {
+            $valid_ctr += 1;
+        }
+    }
+
+    if($valid_ctr != 3){
+        return false;
+    }else{
+        return true;
+    }
 }
